d0f86044ce6157ca2bd79a5e7d223f7323b01b17
KevinDaLam <kvnlam23@gmail.com>
2017-05-19 18:33:13 -0400

    Add files via upload
    Added JackPlateChain library to facilitate chaining behaviour between Node and successive Jack Plates

---
+++ JackPlateChain.cpp
@@ -0,0 +1,80 @@
+/*
+ * JackPlateChain.cpp - Library for Daisy Chain Reaction for Dendrite
+ * Created By Kevin Lam, May 19, 2017
+ * Released for IU Summer Camp
+ */
+
+#include "JackPlateChain.h"
+
+JackPlateChain::JackPlateChain(int port){
+  
+  initializedPort = port;
+
+  //Initializes timer values
+  currentTime = millis();
+  for (int i = 0; i < 4; i++){
+    previousTimeSinceActuation[i] = currentTime - restingDelay;  //To ensure the first actuation does not have to occur after time denoted by restingDelay
+  }
+
+  //Declaring Pins as Outputs for Actuation
+  pinMode(port_pin[initializedPort][0], OUTPUT);
+  pinMode(port_pin[initializedPort][1], OUTPUT);
+  pinMode(port_pin[initializedPort][2], OUTPUT);
+  pinMode(port_pin[initializedPort][3], OUTPUT);
+  
+}
+
+void JackPlateChain::propogateAllActuators(float successiveTime){
+
+  //Checks if time is positive to avoid issues with delay, defaults to 0
+  if(successiveTime < 0){
+    successiveTime = 0;
+  }
+
+  //Checks if actuator has rested, then turns it on for TIME_ON, then turns on the next actuator while powering of the current one
+  for (int i = 0; i < 4; i++){
+
+    if ((*this).hasRestTimeElapsed(i)){
+      digitalWrite(port_pin[initializedPort][i], HIGH);
+    }
+    delay(TIME_ON);
+    digitalWrite(port_pin[initializedPort][i], LOW);
+    delay(successiveTime);
+  }
+  
+}
+
+bool JackPlateChain::checkIRSensorActivated(int sensorNumber){
+
+  sensorNumber += 3; //For proper pin mapping. I.E. First sensor plugged in is pin number 5 on the port. Pin number 5 has an index of 4.
+
+  //Checks if IR Sensor is triggered
+  if (analogRead(port_pin[initializedPort][sensorNumber] > PROXIMITY_RANGE)){
+      
+      return true;
+      
+  }
+
+  return false;
+}
+
+int JackPlateChain::rawIRSensorValue(int sensorNumber){
+
+  sensorNumber += 3; //For proper pin mapping. I.E. First sensor plugged in is pin number 5 on the port. Pin number 5 has an index of 4.
+
+  return analogRead(port_pin[initializedPort][sensorNumber]);
+  
+}
+
+bool JackPlateChain::hasRestTimeElapsed(int actuatorNumber){
+
+  //Checks if time from last actuation is greater than restingDelay
+  currentTime = millis();
+  if (currentTime - previousTimeSinceActuation[actuatorNumber] > restingDelay) {
+    previousTimeSinceActuation[actuatorNumber] = currentTime;
+    return true;
+  }
+  return false;
+  
+}
+
---
+++ JackPlateChain.h
@@ -0,0 +1,77 @@
+/*
+ * JackPlateChain.h - Library for Daisy Chain Reaction for Dendrite
+ * Created By Kevin Lam, May 19, 2017
+ * Released for IU SummerCamp
+ */
+
+#ifndef JackPlateChain_h
+#define JackPlateChain_h
+#include <Arduino.h>        //Include for Pin Mapping on Teensy
+
+// Pin Mappings for Node 3.2, Index starting at 0. I.E. Pin number 1 on P0 Port is port_pin[0][0]
+const int port_pin[4][8] = {{3,  4,  5, 20, A16, A11, A2, A3},
+                                       {25, 32, 6, 21, A17, A13, A15, A20},
+                                       {9, 10, 22, 23,  14,  A1, A18, A19},
+                                       {7,  8,  6, 11,  12,  13, 18, 19}};
+
+
+class JackPlateChain { //Class to dictate behaviour of daisy-chained Jack Plates
+
+  private:
+    const int TIME_ON = 1000;           //Time in milliseconds turned on for actuator on Jack Plate
+    const int PROXIMITY_RANGE = 500;    //Minimum value needed to trigger a detection for sensor on Jack Plate
+    int initializedPort;                //Corresponding port on Node
+    int restingDelay = 30000;           //Resting time in milliseconds needed for shape memory alloy to cool down before being contracted again
+    int previousTimeSinceActuation[4];  //Holder for last time actuator was on
+    int currentTime;                    //Holder for current time
+
+  public:
+
+    //Intialize object to corresponding to appropriate port on Node
+    JackPlateChain (int port);
+
+    //Takes time in seconds, turns on first SMA, then waits successiveTime, then turns on next. Input 0 as parameter for quick succession
+    void propogateAllActuators (float successiveTime);
+
+    //Takes position number of Jack Plate/IRSensor, returns if the specified sensor is activated past PROXIMITY_RANGE. Sensor Number 1 corresponds to first Jack Plate connected to Node, and so on...
+    bool checkIRSensorActivated(int sensorNumber);
+
+    //Takes position number of Jack Plate/IRSensor, returns raw value from IR Sensor. Sensor Number 1 corresponds to first Jack Plate connected to Node, and so on...
+    int rawIRSensorValue(int sensorNumber);
+
+
+    //Checks if time from last actuation of position number has exceeded restingDelay
+    bool hasRestTimeElapsed(int actuatorNumber);
+
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
